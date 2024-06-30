4d06921edaf34195a09cd913c276928758d86a00
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-06-30 10:08:05 +0530

    ranking top

--- frontend/components/NotTop3.jsx
+++ frontend/components/NotTop3.jsx
@@ -20,10 +20,18 @@ const NotTop3 = ({ data }) => {
           alignItems: "center",
         }}
       >
-        <p style={{ width: "5%" }}>Rank</p>
-        <p style={{ width: "65%" }}>Name</p>
-        <p style={{ width: "20%" }}>ID</p>
-        <p style={{ width: "10%" }}>Points</p>
+        <p style={{ width: "5%" }}>
+          <b>Rank</b>
+        </p>
+        <p style={{ width: "65%" }}>
+          <b>Name</b>
+        </p>
+        <p style={{ width: "20%" }}>
+          <b>ID</b>
+        </p>
+        <p style={{ width: "10%" }}>
+          <b>Points</b>
+        </p>
       </div>
       {data.map((item, i) => (
         <div
--- frontend/components/Top3.jsx
+++ frontend/components/Top3.jsx
@@ -18,16 +18,22 @@ const Top3 = ({ data }) => {
             height: "45vh",
             borderRadius: "10px",
             padding: "10px",
+            display: "flex",
+            flexDirection: "column",
           }}
         >
-          <img src={First} style={{ height: "70%" }}></img>
+          <img src={First} style={{ height: "80%", alignSelf: "center" }}></img>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
-            <p>A Pretty Dang Long Name</p>
+            <p>
+              <b>A Pretty Dang Long Name</b>
+            </p>
             <p>Points</p>
           </div>
           <div style={{ display: "flex", justifyContent: "space-between" }}>
             <p>{item[2]}</p>
-            <p>42069</p>
+            <p>
+              <b>42069</b>
+            </p>
           </div>
           <br />
         </div>
