f5203f8a40eb1a112aa46e263191f3ca6af6367a
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-07-14 09:31:56 +0530

    url updated

--- frontend/api/fetchStats.js
+++ frontend/api/fetchStats.js
@@ -1,5 +1,5 @@
 export const fetchStats = async () => {
-  const resp = await fetch("http://localhost:3001");
+  const resp = await fetch("https://campusambassadorbackend.vercel.app/");
   const data = await resp.json();
   data.sort((a, b) => parseInt(b[7]) - parseInt(a[7]));
   console.log(data);
